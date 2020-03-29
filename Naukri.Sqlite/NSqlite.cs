81354a478d8b10514b4261954a2a4bf5d3898158
Naukri <50033668+naukri7707@users.noreply.github.com>
2020-03-29 22:56:23 +0800

    加入 static class NSqlite 用來存取通用變數

---
+++ Naukri.Sqlite/NSqlite.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Naukri.Sqlite
+{
+    public static class NSqlite
+    {
+        public static Action<string> Log { get; set; }
+    }
+}
