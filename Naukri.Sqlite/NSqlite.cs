d847635485d3a90e8168e42e313496ab94ea5a21
Naukri <50033668+naukri7707@users.noreply.github.com>
2020-04-01 02:25:48 +0800

    更新 資料表建立方式

--- Naukri.Sqlite/NSqlite.cs
+++ Naukri.Sqlite/NSqlite.cs
@@ -14,6 +14,72 @@ namespace Naukri.Sqlite
 
         public static NSqliteOption Option = NSqliteOption.CheckSchema;
 
-        public static Dictionary<string, NSqliteDatabase> Database { get; } = new Dictionary<string, NSqliteDatabase>();
+        public static void CreateTable<Table>(string connectionText)
+        {
+            NSqliteTableInfo.SetTableInfo<Table>(connectionText);
+        }
+
+        public static void CreateTable<Table1, Table2>(string connectionText)
+        {
+            NSqliteTableInfo.SetTableInfo<Table1>(connectionText);
+            NSqliteTableInfo.SetTableInfo<Table2>(connectionText);
+        }
+
+        public static void CreateTable<Table1, Table2, Table3>(string connectionText)
+        {
+            NSqliteTableInfo.SetTableInfo<Table1>(connectionText);
+            NSqliteTableInfo.SetTableInfo<Table2>(connectionText);
+            NSqliteTableInfo.SetTableInfo<Table3>(connectionText);
+        }
+
+        public static void CreateTable<T1, T2, T3, T4>(string connectionText)
+        {
+            NSqliteTableInfo.SetTableInfo<T1>(connectionText);
+            NSqliteTableInfo.SetTableInfo<T2>(connectionText);
+            NSqliteTableInfo.SetTableInfo<T3>(connectionText);
+            NSqliteTableInfo.SetTableInfo<T4>(connectionText);
+        }
+
+        public static void CreateTable<T1, T2, T3, T4, T5>(string connectionText)
+        {
+            NSqliteTableInfo.SetTableInfo<T1>(connectionText);
+            NSqliteTableInfo.SetTableInfo<T2>(connectionText);
+            NSqliteTableInfo.SetTableInfo<T3>(connectionText);
+            NSqliteTableInfo.SetTableInfo<T4>(connectionText);
+            NSqliteTableInfo.SetTableInfo<T5>(connectionText);
+        }
+
+        public static void CreateTable<T1, T2, T3, T4, T5, T6>(string connectionText)
+        {
+            NSqliteTableInfo.SetTableInfo<T1>(connectionText);
+            NSqliteTableInfo.SetTableInfo<T2>(connectionText);
+            NSqliteTableInfo.SetTableInfo<T3>(connectionText);
+            NSqliteTableInfo.SetTableInfo<T4>(connectionText);
+            NSqliteTableInfo.SetTableInfo<T5>(connectionText);
+            NSqliteTableInfo.SetTableInfo<T6>(connectionText);
+        }
+
+        public static void CreateTable<T1, T2, T3, T4, T5, T6, T7>(string connectionText)
+        {
+            NSqliteTableInfo.SetTableInfo<T1>(connectionText);
+            NSqliteTableInfo.SetTableInfo<T2>(connectionText);
+            NSqliteTableInfo.SetTableInfo<T3>(connectionText);
+            NSqliteTableInfo.SetTableInfo<T4>(connectionText);
+            NSqliteTableInfo.SetTableInfo<T5>(connectionText);
+            NSqliteTableInfo.SetTableInfo<T6>(connectionText);
+            NSqliteTableInfo.SetTableInfo<T7>(connectionText);
+        }
+
+        public static void CreateTable<T1, T2, T3, T4, T5, T6, T7, T8>(string connectionText)
+        {
+            NSqliteTableInfo.SetTableInfo<T1>(connectionText);
+            NSqliteTableInfo.SetTableInfo<T2>(connectionText);
+            NSqliteTableInfo.SetTableInfo<T3>(connectionText);
+            NSqliteTableInfo.SetTableInfo<T4>(connectionText);
+            NSqliteTableInfo.SetTableInfo<T5>(connectionText);
+            NSqliteTableInfo.SetTableInfo<T6>(connectionText);
+            NSqliteTableInfo.SetTableInfo<T7>(connectionText);
+            NSqliteTableInfo.SetTableInfo<T8>(connectionText);
+        }
     }
 }
